710d48a9ce58b5def351e538db9bf8be0f510a62
Lajos Kossuth <kossuthl4jos@gmail.com>
2019-11-19 12:40:46 +0100

    simplify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,3 @@
 module.exports = {
-	presets: [
-		['@babel/preset-env', {
-			targets: {
-				browsers: ['last 2 versions', 'ie >= 11']
-			},
-			modules: false
-		}]
-	],
-	env: {
-		test: {
-			presets: [
-					[
-							'@babel/preset-env',
-							{
-									modules: 'commonjs',
-									debug: false
-							}
-					],
-					'@babel/preset-react'
-			],
-			plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					'@babel/plugin-proposal-class-properties'
-			]
-		},
-	}
+	presets: ['@babel/preset-env', '@babel/preset-react'],
 };
