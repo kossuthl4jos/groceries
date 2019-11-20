4ae276929286b0ebbe5e5ae1bac614a15b502c34
Lajos Kossuth <kossuthl4jos@gmail.com>
2019-11-20 16:41:36 +0100

    change data structure

--- src/gateway/fake-gateway.js
+++ src/gateway/fake-gateway.js
@@ -1,19 +1,19 @@
 const sandwich = [
 	{
-		id: 1,
+		id: '1',
 		name: 'Sandwich',
 		items: [
 			{
 				itemId: '1s',
 				name: 'Bread',
-				completed: 'yes',
+				completed: true,
 				completedBy: 'Robert',
 				price: 3
 			},
 			{
 				itemId: '2s',
 				name: 'Butter',
-				completed: 'no',
+				completed: false,
 				completedBy: '',
 				price: null
 			}
@@ -23,20 +23,20 @@ const sandwich = [
 
 const bolognese = [
 	{
-		id: 2,
+		id: '2',
 		name: 'Bolognese',
 		items: [
 			{
 				itemId: '1b',
 				name: 'Pasta',
-				completed: 'yes',
+				completed: true,
 				completedBy: 'Louis',
 				price: 4
 			},
 			{
 				itemId: '2b',
 				name: 'Tomato',
-				completed: 'no',
+				completed: false,
 				completedBy: '',
 				price: null
 			}
