9b48f4bf32f047d11b6ceee2cf39e74777b8ff60
Lajos Kossuth <kossuthl4jos@gmail.com>
2019-11-22 11:00:44 +0100

    refactor Items

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import ItemsToBuy from './ItemsToBuy';
-import CompletedItems from './CompletedItems';
-
 
 export class Items extends Component {
 	render() {
 		return (
-			<section className="section">
-				<ItemsToBuy items={ this.props.items } />
-				<CompletedItems items={ this.props.items } />
-			</section>
+			<div>
+				{
+					this.props.items.map((item) => ( !item.completed &&
+						<div key={ item.itemId }>
+							<div className="item has-text-left">
+									{ item.name }
+									<div>
+										<i class="far fa-check-square"></i>
+									</div>
+							</div>
+						</div>
+					))
+				}
+				{
+					this.props.items.map((item) => ( item.completed &&
+						<div key={ item.itemId }>
+							<div className="has-text-left"> completed </div>
+							<div className="item has-text-left" style={{ textDecoration: "line-through"}}>
+									{ item.name }
+									<input style={{ float:"right" }} type="checkbox" />
+							</div>
+						</div>
+					))
+				}
+			</div>
 		)
 	}
 }
