51a132ed728a1b97b2cd76cf37d6b317847b5180
Lajos Kossuth <kossuthl4jos@gmail.com>
2019-11-22 18:43:56 +0100

    add new component

---
+++ src/components/Statistics.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Statistics extends Component {
+	render() {
+		return (
+			<div>
+				Hello new component
+			</div>
+		)
+	}
+}
+
+export default Statistics
