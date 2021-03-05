3a0da5ba8ff43829c3b460b420590d990ee9036a
Lajos <kossuthl4jos@gmail.com>
2021-03-05 18:35:24 +0100

    update Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,77 +1,66 @@
-import React, { useState } from 'react'
-import { Link, Redirect } from 'react-router-dom';
-import { signIn } from '../gateway/fake-gateway'
+import React, { FormEvent, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signIn } from '../gateway/fake-gateway';
 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Alert from 'react-bootstrap/Alert';
+import { Button, Form, Alert, FormGroup, FormLabel, FormControl, FormText } from 'react-bootstrap';
 
-import { useAuth } from "../context/auth";
+import { useAuth } from '../context/auth';
 
 export const Login = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
   const [isError, setIsError] = useState(false);
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   const { setAuthTokens } = useAuth();
 
-	function postLogin() {
-		try {
-			const authTokens = signIn({ userName, password });
-			setAuthTokens({ authTokens });
-			setLoggedIn(true);
-		}
-		catch {
-			setIsError(true);
-		}
-	}
-
-	if (isLoggedIn) {
-    return <Redirect to="/" />;
+  function postLogin() {
+    try {
+      const authTokens = signIn({ userName, password });
+      setAuthTokens({ authTokens });
+      navigate('/');
+    } catch {
+      setIsError(true);
+    }
   }
 
-	return (
-		<div>
-			<Form className="main-component">
-				<Form.Group controlId="formUserName">
-					<Form.Label>Username</Form.Label>
-					<Form.Control
-						type="username"
-						placeholder="Username"
-						onChange={ e => {
-							setUserName(e.target.value);
-						}}
-						value={ userName }/>
-				</Form.Group>
+  return (
+    <div>
+      <Form className="main-component">
+        <FormGroup controlId="formUserName">
+          <FormLabel>Username</FormLabel>
+          <FormControl
+            type="username"
+            placeholder="Username"
+            onChange={(e: FormEvent<HTMLInputElement>) => {
+              setUserName((e.target as HTMLInputElement).value);
+            }}
+            value={userName}
+          />
+        </FormGroup>
 
-				<Form.Group controlId="formBasicPassword">
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						type="password"
-						placeholder="Password"
-						onChange={e => {
-							setPassword(e.target.value);
-						}}
-						value={ password }/>
-				</Form.Group>
+        <FormGroup controlId="formBasicPassword">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            type="password"
+            placeholder="Password"
+            onChange={(e: FormEvent<HTMLInputElement>) => {
+              setPassword((e.target as HTMLInputElement).value);
+            }}
+            value={password}
+          />
+        </FormGroup>
 
-				<Button
-					variant="primary"
-					type="submit"
-					onClick={ postLogin }>
-					Sign in
-				</Button>
+        <Button variant="primary" type="submit" onClick={postLogin}>
+          Sign in
+        </Button>
 
-				<Link to="/signup">
-					<Form.Text className="text-muted">
-						Don't have an account yet?
-					</Form.Text>
-				</Link>
-				{
-					isError &&
-					<Alert variant="danger">The username or password provided were incorrect!</Alert>
-				}
-			</Form>
-		</div>
-	)
-}
+        <Link to="/signup">
+          <FormText className="text-muted">Don't have an account yet?</FormText>
+        </Link>
+        {isError && (
+          <Alert variant="danger">The username or password provided were incorrect!</Alert>
+        )}
+      </Form>
+    </div>
+  );
+};
