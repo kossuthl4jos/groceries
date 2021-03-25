9f5a9d3d0e1c5f635f4369ad94520397534135c6
Lajos <kossuthl4jos@gmail.com>
2021-03-25 19:14:34 +0100

    activate button in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
   return (
     <div className="navigation-bar">
-      <Link className="nav-button" to="/">
+      <Link
+        className={`nav-button ${window.location.pathname === '/lists' ? 'active' : ''}`}
+        to="/lists">
         <i className="fas fa-list" />
         List
       </Link>
-      <Link className="nav-button" to="/stats">
+      <Link
+        className={`nav-button ${window.location.pathname === '/stats' ? 'active' : ''}`}
+        to="/stats">
         <i className="fas fa-chart-pie" />
         Stats
       </Link>
