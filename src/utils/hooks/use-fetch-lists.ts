7761a1a0aa28fc360dd51f6c3bcd172390ae5244
Lajos <kossuthl4jos@gmail.com>
2021-03-25 09:15:17 +0100

    fix reactivity

--- src/utils/hooks/use-fetch-lists.ts
+++ src/utils/hooks/use-fetch-lists.ts
@@ -5,14 +5,22 @@ const TOKEN_KEY = 'groceries-lists';
 
 export function useFetchLists(): {
   lists: Array<List>;
+  reFetchLists: VoidFunction;
 } {
   const [lists, setLists] = useState<Array<List>>([]);
 
+  const groceriesList = localStorage.getItem(TOKEN_KEY) ?? '{ lists: [] }';
+
+  const fetchLists = () => {
+    const groceriesList = localStorage.getItem(TOKEN_KEY) ?? '{ lists: [] }';
+    const { lists } = JSON.parse(groceriesList);
+    setLists(lists);
+  };
+
   useEffect(() => {
-    const groceriesList = localStorage.getItem(TOKEN_KEY);
     const { lists } = JSON.parse(groceriesList != null ? groceriesList : '{ lists: [] }');
     setLists(lists);
-  }, []);
+  }, [groceriesList]);
 
-  return { lists };
+  return { lists, reFetchLists: fetchLists };
 }
--- src/utils/hooks/use-save-lists.ts
+++ src/utils/hooks/use-save-lists.ts
@@ -1,11 +1,15 @@
 import { List } from '~/types';
+import { useFetchLists } from './use-fetch-lists';
 
 const TOKEN_KEY = 'groceries-lists';
 
 export function useSaveLists(): { saveLists: (lists: Array<List>) => void } {
+  const { reFetchLists } = useFetchLists();
+
   function saveLists(lists: Array<List>) {
     if (lists != null) {
       localStorage.setItem(TOKEN_KEY, JSON.stringify({ lists }));
+      reFetchLists();
     }
   }
 
