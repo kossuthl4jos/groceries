9808c91fc78c95142d271c242d1a6be7eeee8b0b
Lajos <kossuthl4jos@gmail.com>
2021-04-01 22:35:17 +0200

    make gateway functions async

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -2,28 +2,21 @@ import { List } from '~/types';
 import { LocalGateWay } from './localGateway';
 import { RemoteGateWay } from './remoteGateway';
 
-// export interface IGateway {
-//   getLists: () => Array<List>;
-//   addList: (list: List) => void;
-//   deleteList: (listId: string) => void;
-//   updateList: (list: List) => void;
-// }
-
 const backendMode = process.env.BACKEND;
 const backendService = backendMode === 'local' ? new LocalGateWay() : new RemoteGateWay();
 
 export async function getLists(): Promise<Array<List>> {
-  return backendService.getLists();
+  return await backendService.getLists();
 }
 
-export function addList(list: List): void {
-  backendService.addList(list);
+export async function addList(list: List): Promise<void> {
+  await backendService.addList(list);
 }
 
-export function deleteList(listId: string): void {
-  backendService.deleteList(listId);
+export async function deleteList(listId: string): Promise<void> {
+  await backendService.deleteList(listId);
 }
 
-export function updateList(list: List): void {
-  backendService.updateList(list);
+export async function updateList(list: List): Promise<void> {
+  await backendService.updateList(list);
 }
